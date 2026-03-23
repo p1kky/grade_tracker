3f6f162ba3564ce73024463fdc3de04d6708bb94
p1kky <maxim.pep3@gmail.com>
2026-03-23 20:39:16 +0300

    match-case to if-else

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cli_interface
+import config
 import data_manager
 
-data_manager.generate_empty_json(cli_interface.subjects)
+data_manager.generate_empty_json(config.SUBJECTS)
 data = data_manager.load_json()
 
 intro_text = "\n" * 3 + "-*- " * 3 + "Grade Tracker" + " -*-" * 3
@@ -18,38 +19,33 @@ def main():
 
     print(intro_text)
 
-    match choice_after_quarter:
-        case 1:
-            user_subject = cli_interface.get_subject()
-            print(f"Вы выбрали предмет: {user_subject}")
+    if choice_after_quarter == 1:
+        user_subject = cli_interface.get_subject()
 
-            print(intro_text)
+        print(intro_text)
 
-            choice_after_subject = cli_interface.aftersubject_choice()
+        choice_after_subject = cli_interface.aftersubject_choice()
 
-            match choice_after_subject:
-                case 1:
-                    cli_interface.show_marks(data, school_quarter, user_subject)
+        if choice_after_subject == 1:
+            cli_interface.show_marks(data, school_quarter, user_subject)
 
-                case 2:
-                    cli_interface.add_mark(data, school_quarter, user_subject)
+        elif choice_after_subject == 2:
+            cli_interface.add_mark(data, school_quarter, user_subject)
 
-                case 3:
-                    cli_interface.remove_mark(data, school_quarter, user_subject)
+        elif choice_after_subject == 3:
+            cli_interface.remove_mark(data, school_quarter, user_subject)
 
-                case 4:
-                    cli_interface.reset_subject_marks(
-                        data, school_quarter, user_subject
-                    )
+        elif choice_after_subject == 4:
+            cli_interface.reset_subject_marks(data, school_quarter, user_subject)
 
-                case 5:
-                    cli_interface.print_quarter_mark(data, school_quarter, user_subject)
+        elif choice_after_subject == 5:
+            cli_interface.print_quarter_mark(data, school_quarter, user_subject)
 
-        case 2:
-            cli_interface.get_gpa(data, school_quarter)
+    elif choice_after_quarter == 2:
+        cli_interface.get_gpa(data, school_quarter)
 
-        case 3:
-            cli_interface.delete_quarter_marks(data, school_quarter)
+    elif choice_after_quarter == 3:
+        cli_interface.delete_quarter_marks(data, school_quarter)
 
 
 if __name__ == "__main__":
